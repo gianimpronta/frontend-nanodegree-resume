74ee03ddab9089ee2f116f75b6ada3301cc71698
gianimpronta <gian.impronta@gmail.com>
2017-12-19 16:49:00 -0200

    Added some more info in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,12 +9,13 @@ var bio = {
     "role": "Cientista de dados",
     "contacts": {
         "mobile": "(21) 99252-3338",
-        "emeila": "gian.impronta@gmail.com",
-        "github": "gianimpronta"
+        "email": "gian.impronta@gmail.com",
+        "github": "gianimpronta",
+        "location": "Rio de janeiro"
     },
     "picture": "images/fry.jpg",
     "message": "Welcome",
-    "skill": "R, Python, JS"
+    "skill": "R, Python, JavaScript, Oracle, Linux, Html, CSS",
 };
 
 var work = {
@@ -32,14 +33,61 @@ var education = {
     schools: [
         {
             "name": "ABMDPII",
-            "dates": "10/04/2010 to 02/12/2012",
+            "dates": "10/04/2010-02/12/2012",
             "degree": "graduate",
             "location": "Rio de Janeiro",
-            "majors":
+            "majors": [
+                "military",
+                "Firefighter",
+                "physics",
+            ]
+        },
+        {
+            "name": "INFNET",
+            "dates": "01/07/2016-31/06/2020",
+            "degree": "graduate",
+            "location": "Rio de Janeiro",
+            "majors": [
+                "Bussiness Inteligence",
+                "Data Science",
+                "Databases",
+                "Data Warehouse"
+            ]
+        }
+    ],
+    onlineCourses : [
+        {
+            "title": "Getting and Cleaning Data",
+            "school": "John Hopkins University",
+            "dates": "21/09/2016",
+            "url": "https://www.coursera.org/account/accomplishments/certificate/YQBWYA7LV8CK"
+        },
+        {
+            "title": "R Programming",
+            "school": "John Hopkins University",
+            "dates": "24/06/2016",
+            "url": "https://www.coursera.org/account/accomplishments/certificate/HFJ8SWB8SV9E"
+        },
+        {
+            "title": "The Data Scientist’s Toolbox",
+            "school": "John Hopkins University",
+            "dates": "24/06/2016",
+            "url": "https://www.coursera.org/account/accomplishments/certificate/6HDW2RUCSCN2"
+        },
+        {
+            "title": "Intro into Programming",
+            "school": "Udacity",
+            "dates": "21/09/2017",
+            "url": "https://www.udacity.com"
         }
     ]
 
+
+
+
 };
+
+
 
 $("#main").append(work.position);
 $("#main").append(education.name);
