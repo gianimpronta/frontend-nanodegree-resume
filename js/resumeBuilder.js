abe0a9a9368fdaa9bd83420645030a4216ca3b5b
gianimpronta <gian.impronta@gmail.com>
2017-12-19 16:11:52 -0200

    Completing resumeBuilder and index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,45 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Gianpaolo Impronta");
+var formattedRole = HTMLheaderRole.replace("%data%", "Cientista de Dados");
+var formattedEmail = HTMLemail.replace("%data%", "gian.impronta@gmail.com");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+var bio = {
+    "name": "Gianpaolo Martins Impronta",
+    "role": "Cientista de dados",
+    "contacts": {
+        "mobile": "(21) 99252-3338",
+        "emeila": "gian.impronta@gmail.com",
+        "github": "gianimpronta"
+    },
+    "picture": "images/fry.jpg",
+    "message": "Welcome",
+    "skill": "R, Python, JS"
+};
+
+var work = {
+    "jobs": [
+        {
+            "title": "Analista de Projetos",
+            "employer": "CBMERJ",
+            "dates": "From 10/04/2010 until now",
+            "city": "Rio de janeiro"
+        }
+    ]
+};
+
+var education = {
+    schools: [
+        {
+            "name": "ABMDPII",
+            "dates": "10/04/2010 to 02/12/2012",
+            "degree": "graduate",
+            "location": "Rio de Janeiro",
+            "majors":
+        }
+    ]
+
+};
+
+$("#main").append(work.position);
+$("#main").append(education.name);
